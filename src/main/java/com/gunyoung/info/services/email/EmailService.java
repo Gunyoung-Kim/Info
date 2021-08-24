e3ba005347bbcac58081f309bc9052b8052ace7c
Gunyoung-Kim <gun025bba@naver.com>
2021-08-24 16:26:08 +0900

    테스트 코드 추가: EmailServiceImpl에 대한 단위 테스트 코드 추가

--- src/main/java/com/gunyoung/info/services/email/EmailService.java
+++ src/main/java/com/gunyoung/info/services/email/EmailService.java
@@ -3,5 +3,10 @@ package com.gunyoung.info.services.email;
 import com.gunyoung.info.dto.email.EmailDTO;
 
 public interface EmailService {
+	
+	/**
+	 * {@link EmailDTO}에 담긴 정보를 통해 email 전송
+	 * @author kimgun-yeong
+	 */
 	public void sendEmail(EmailDTO email);
 }
