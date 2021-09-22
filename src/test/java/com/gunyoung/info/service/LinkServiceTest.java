df7ec37c44ad32b5491283fb4a7c988a8461a8c5
Gunyoung-Kim <gun025bba@naver.com>
2021-09-22 23:01:09 +0900

    테스트 코드 추가: LinkService에 대한 통합 테스트 코드 추가

---
+++ src/test/java/com/gunyoung/info/service/LinkServiceTest.java
@@ -0,0 +1,319 @@
+package com.gunyoung.info.service;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertNull;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.gunyoung.info.domain.Content;
+import com.gunyoung.info.domain.Link;
+import com.gunyoung.info.dto.LinkDTO;
+import com.gunyoung.info.repos.ContentRepository;
+import com.gunyoung.info.repos.LinkRepository;
+import com.gunyoung.info.services.domain.LinkService;
+import com.gunyoung.info.testutil.Integration;
+import com.gunyoung.info.util.ContentTest;
+import com.gunyoung.info.util.LinkTest;
+
+/**
+ * {@link LinkService} 에 대한 테스트 클래스 <br>
+ * 테스트 범위:(통합 테스트) 서비스 계층 - 영속성 계층 
+ * @author kimgun-yeong
+ *
+ */
+@Integration
+@SpringBootTest
+public class LinkServiceTest {
+	
+	@Autowired
+	LinkRepository linkRepository;
+	
+	@Autowired
+	ContentRepository contentRepository;
+	
+	@Autowired
+	LinkService linkService;
+	
+	private Link link;
+	
+	@BeforeEach
+	void setup() {
+		link = LinkTest.getLinkInstance();
+		linkRepository.save(link);
+	}
+	
+	@AfterEach
+	void tearDown() {
+		linkRepository.deleteAll();
+	}
+	
+	/*
+	 * public Link findById(Long id)
+	 */
+	
+	@Test
+	@DisplayName("ID로 Link 찾기 -> 존재하지 않음")
+	public void findByIdTestNonExist() {
+		//Given
+		Long nonExistId = LinkTest.getNonExistLinkId(linkRepository);
+		
+		//When
+		Link result = linkService.findById(nonExistId);
+		
+		//Then
+		assertNull(result);
+	}
+	
+	@Test
+	@DisplayName("ID로 Link 찾기 -> 정상")
+	public void findByIdTest() {
+		//Given
+		Long linkId = link.getId();
+		
+		//When
+		Link result = linkService.findById(linkId);
+		
+		//Then
+		assertEquals(linkId, result.getId());
+	}
+	
+	/*
+	 * public List<Link> findAllByContentId(Long contentId)
+	 */
+	
+	@Test
+	@Transactional
+	@DisplayName("Content Id를 만족하는 모든 Link 반환 -> 정상")
+	public void findAllByContentIdTest() {
+		//Given
+		int newLinkNum = 10;
+		addNewLinks(newLinkNum);
+		
+		Content content = ContentTest.getContentInstance();
+		contentRepository.save(content);
+		setContentToAllLinks(content);
+		
+		Long contentId = content.getId();
+		long allLinkNum = linkRepository.count();
+		
+		//When
+		List<Link> result = linkService.findAllByContentId(contentId);
+		
+		//Then
+		verifyLinksNumAndThereContents(allLinkNum, content, result);
+	}
+	
+	private void addNewLinks(int newLinkNum) {
+		List<Link> newLinks = new ArrayList<>();
+		for(int i=0;i<newLinkNum;i++) {
+			Link link = LinkTest.getLinkInstance();
+			newLinks.add(link);
+		}
+		linkRepository.saveAll(newLinks);
+	}
+	
+	private void setContentToAllLinks(Content content) {
+		List<Link> links = linkRepository.findAll();
+		for(Link link: links) {
+			link.setContent(content);
+		}
+		linkRepository.saveAll(links);
+	}
+	
+	private void verifyLinksNumAndThereContents(long allLinksNum, Content content, List<Link> result) {
+		assertEquals(allLinksNum, result.size());
+		for(Link link: result) {
+			assertEquals(content, link.getContent());
+		}
+	}
+	
+	/*
+	 * public Link save(Link link)
+	 */
+	
+	@Test
+	@DisplayName("Link 생성 및 수정 -> 수정, link url 수정") 
+	public void saveTestModifyURL() {
+		//Given
+		String changeURL = "https://change.com";
+		link.setUrl(changeURL);
+		
+		Long linkId = link.getId();
+		
+		//When
+		linkService.save(link);
+		
+		//Then
+		assertEquals(changeURL, linkRepository.findById(linkId).get().getUrl());
+	}
+	
+	@Test
+	@DisplayName("Link 생성 및 수정 -> 새로운 Link 추가")
+	public void saveTestAddNewLink() {
+		//Given
+		Link newLink = LinkTest.getLinkInstance();
+		
+		long beforeLinkNum = linkRepository.count();
+		
+		//When
+		linkService.save(newLink);
+		
+		//Then
+		assertEquals(beforeLinkNum + 1 , linkRepository.count());
+	}
+	
+	/*
+	 * public List<Link> updateLinksForContent(Content content, Iterable<LinkDTO> linkDTOs, Iterable<Link> existContentLinks)
+	 */
+	
+	@Test
+	@Transactional
+	@DisplayName("기존의 Content의 Link들을 LinkDTO를 통해 업데이트 -> 정상, Content의 기존 Link 삭제확인")
+	public void updateLinksForContentTestCheckDeleteLink() {
+		//Given
+		Content content = getContentForLink();
+		
+		List<LinkDTO> emptyLinkDTOList = new ArrayList<>();
+		
+		Long linkId = link.getId();
+		//When
+		linkService.updateLinksForContent(content, emptyLinkDTOList, content.getLinks());
+		
+		//Then
+		assertFalse(linkRepository.existsById(linkId));
+	}
+	
+	@Test
+	@Transactional
+	@DisplayName("기존의 Content의 Link들을 LinkDTO를 통해 업데이트 -> 정상, 기존의 Link 변경, URL 변경")
+	public void updateLinksForContentModifyLink() {
+		//Given
+		String changeURL = "https://change.com";
+		Content content = getContentForLink();
+		
+		List<LinkDTO> linkDTOList = new ArrayList<>();
+		
+		LinkDTO linkDTO = LinkDTO.builder()
+				.id(link.getId())
+				.tag(link.getTag())
+				.url(changeURL)
+				.build();
+		linkDTOList.add(linkDTO);
+		
+		Long linkId = link.getId();
+		
+		//When
+		linkService.updateLinksForContent(content, linkDTOList, content.getLinks());
+		
+		//Then
+		assertEquals(changeURL, linkRepository.findById(linkId).get().getUrl());
+	}
+	
+	@Test
+	@Transactional
+	@DisplayName("기존의 Content의 Link들을 LinkDTO를 통해 업데이트 -> 정상, 새로운 Link 추가")
+	public void updateLinksForContentTestAddLink() {
+		//Given
+		Content content = getContentForLink();
+		String newLinkTag = "newTag";
+		String newLinkURL = "newurl.com";
+		
+		List<LinkDTO> linkDTOList = new ArrayList<>();
+		
+		LinkDTO linkDTO = LinkDTO.builder()
+				.tag(newLinkTag)
+				.url(newLinkURL)
+				.build();
+		linkDTOList.add(linkDTO);
+		
+		long beforeLinkNum = linkRepository.count();
+		
+		//When
+		linkService.updateLinksForContent(content, linkDTOList, content.getLinks());
+		
+		//Then
+		assertEquals(beforeLinkNum + 1, linkRepository.count() + 1);
+	}
+	
+	private Content getContentForLink() {
+		Content content = ContentTest.getContentInstance();
+		contentRepository.save(content);
+		
+		link.setContent(content);
+		content.getLinks().add(link);
+		linkRepository.save(link);
+		return content;
+	}
+	
+	/*
+	 * public List<Link> saveAll(Iterable<Link> links)
+	 */
+	
+	@Test
+	@DisplayName("모든 Link들 저장 -> 정상")
+	public void saveAllTest() {
+		//Given
+		long beforeLinkNum = linkRepository.count();
+		long newLinksNum = 6;
+		List<Link> newLinks = getLinksByGivenNum(newLinksNum);
+		
+		//When
+		linkService.saveAll(newLinks);
+		
+		//Then
+		assertEquals(beforeLinkNum + newLinksNum,linkRepository.count());
+	}
+	
+	private List<Link> getLinksByGivenNum(long newLinksNum) {
+		List<Link> newLinks = new ArrayList<>();
+		for(int i=0;i<newLinksNum;i++) {
+			Link link = LinkTest.getLinkInstance();
+			newLinks.add(link);
+		}
+		return newLinks;
+	}
+	
+	/*
+	 * public void delete(Link link)
+	 */
+	
+	@Test
+	@DisplayName("Link 삭제 -> 정상")
+	public void deleteTest() {
+		//Given
+		Long deleteLinkId = link.getId();
+		
+		//When
+		linkService.delete(link);
+		
+		//Then
+		assertFalse(linkRepository.existsById(deleteLinkId));
+	}
+	
+	/*
+	 * public void deleteById(Long id);
+	 */
+	
+	@Test
+	@DisplayName("ID로 Link 삭제 -> 정상")
+	public void deleteByIdTest() {
+		//Given
+		Long deleteLinkId = link.getId();
+		
+		//When
+		linkService.deleteById(deleteLinkId);
+		
+		//Then
+		assertFalse(linkRepository.existsById(deleteLinkId));
+	}
+}
